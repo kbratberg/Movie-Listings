65bb57b8e0145c31084620a0b454e73e4ba0e1f9
Katie Bratberg <57917884+kbratberg@users.noreply.github.com>
2020-09-23 18:34:50 -0500

    file not found exception

--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using NLog.Web;
+using System.IO;
 
-namespace Movie_Listing
+namespace MovieListing
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+
+            // create instance of Logger
+            var logger = NLogBuilder.ConfigureNLog(path).GetCurrentClassLogger();
+            logger.Info("Program started");
+
+            string file = "movies.csv";
+             // make sure movie file exists
+            if (!File.Exists(file))
+            {
+                logger.Error("File does not exist: {File}", file);
+            }
+            else
+            {
+                // TODO: create user menu
+            }
+            
+            logger.Info("Program ended");
         }
     }
 }
