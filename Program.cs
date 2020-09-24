140459e7c5f89aa10b27f201b944c96ade9e0da1
Katie Bratberg <57917884+kbratberg@users.noreply.github.com>
2020-09-23 19:08:21 -0500

    print data to file

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using NLog.Web;
 using System.IO;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MovieListing
 {
@@ -23,12 +24,12 @@ namespace MovieListing
             }
             else
             {
-                 // create parallel lists of movie details
+                // create parallel lists of movie details
                 // lists are used since we do not know number of lines of data
                 List<UInt64> MovieIds = new List<UInt64>();
                 List<string> MovieTitles = new List<string>();
                 List<string> MovieGenres = new List<string>();
-
+                // to populate the lists with data, read from the data file
                 try
                 {
                     StreamReader sr = new StreamReader(file);
@@ -37,7 +38,7 @@ namespace MovieListing
                     while (!sr.EndOfStream)
                     {
                         string line = sr.ReadLine();
-                       // first look for quote(") in string
+                        // first look for quote(") in string
                         // this indicates a comma(,) in movie title
                         int idx = line.IndexOf('"');
                         if (idx == -1)
@@ -53,6 +54,22 @@ namespace MovieListing
                             // replace "|" with ", "
                             MovieGenres.Add(movieDetails[2].Replace("|", ", "));
                         }
+                        else
+                        {
+                            // quote = comma in movie title
+                            // extract the movieId
+                            MovieIds.Add(UInt64.Parse(line.Substring(0, idx - 1)));
+                            // remove movieId and first quote from string
+                            line = line.Substring(idx + 1);
+                            // find the next quote
+                            idx = line.IndexOf('"');
+                            // extract the movieTitle
+                            MovieTitles.Add(line.Substring(0, idx));
+                            // remove title and last comma from the string
+                            line = line.Substring(idx + 2);
+                            // replace the "|" with ", "
+                            MovieGenres.Add(line.Replace("|", ", "));
+                        }
                     }
                     sr.Close();
                 }
@@ -60,9 +77,12 @@ namespace MovieListing
                 {
                     logger.Error(ex.Message);
                 }
+
                 string choice;
                 do
                 {
+                    logger.Info("Movies in file {Count}", MovieIds.Count);
+
                     // display choices to user
                     Console.WriteLine("1) Add Movie");
                     Console.WriteLine("2) Display All Movies");
@@ -75,10 +95,61 @@ namespace MovieListing
                     if (choice == "1")
                     {
                         // Add Movie
+                        // ask user to input movie title
+                        Console.WriteLine("Enter the movie title");
+                        // input title
+                        string movieTitle = Console.ReadLine();
+                        // check for duplicate title
+                        List<string> LowerCaseMovieTitles = MovieTitles.ConvertAll(t => t.ToLower());
+                        if (LowerCaseMovieTitles.Contains(movieTitle.ToLower()))
+                        {
+                            logger.Info("Duplicate movie title {Title}", movieTitle);
+                        }
+                        else
+                        {
+                            // generate movie id - use max value in MovieIds + 1
+                            UInt64 movieId = MovieIds.Max() + 1;
+                            // input genres
+                            List<string> genres = new List<string>();
+                            string genre;
+                            do
+                            {
+                                // ask user to enter genre
+                                Console.WriteLine("Enter genre (or done to quit)");
+                                // input genre
+                                genre = Console.ReadLine();
+                                // if user enters "done"
+                                // or does not enter a genre do not add it to list
+                                if (genre != "done" && genre.Length > 0)
+                                {
+                                    genres.Add(genre);
+                                }
+                            } while (genre != "done");
+                            // specify if no genres are entered
+                            if (genres.Count == 0)
+                            {
+                                genres.Add("(no genres listed)");
+                            }
+                            // use "|" as delimeter for genres
+                            string genresString = string.Join("|", genres);
+                            // if there is a comma(,) in the title, wrap it in quotes
+                            movieTitle = movieTitle.IndexOf(',') != -1 ? $"\"{movieTitle}\"" : movieTitle;
+                            // create file from data
+                            StreamWriter sw = new StreamWriter(file, true);
+                            sw.WriteLine($"{movieId},{movieTitle},{genresString}");
+                            sw.Close();
+                            // add movie details to Lists
+                            MovieIds.Add(movieId);
+                            MovieTitles.Add(movieTitle);
+                            MovieGenres.Add(genresString);
+                            // log transaction
+                            logger.Info("Movie id {Id} added", movieId);
+                        }
                     }
                     else if (choice == "2")
                     {
-                         // loop thru Movie Lists
+                        // Display All Movies
+                        // loop thru Movie Lists
                         for (int i = 0; i < MovieIds.Count; i++)
                         {
                             // display movie details
